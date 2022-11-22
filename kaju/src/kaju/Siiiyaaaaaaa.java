ccde11fee56462f4b4b3eaed21adfbca4699a726
91885 <91885@Punam>
2022-11-22 21:54:37 +0530

    12334

---
+++ kaju/src/kaju/Siiiyaaaaaaa.java
@@ -0,0 +1,13 @@
+package kaju;
+
+public class Siiiyaaaaaaa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("kajuuuuu");
+		System.out.println("katli");
+		
+	}
+
+}
